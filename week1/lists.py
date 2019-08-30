193773cfa2d7e9d3a7b01f53c7ee00f53fa924bd
Xinyi Ding <dxywill@gmail.com>
2019-08-30 09:33:52 -0500

    lecture 3

---
+++ week1/lists.py
@@ -0,0 +1,28 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'apple']
+# motorcycles.append('mazda')
+
+# l1 = [1, 2, 3]
+# l2 = [4, 5, 6]
+# l3 = l1.extend(l2)
+#
+#
+# l3 = l1 + l2
+#
+# print(l1)
+# print(l3)
+# motorcycle_1 = motorcycles.pop(1)
+#
+# print(motorcycle_1)
+# print(motorcycles)
+# motorcycles[0] = 'jeep'
+#print(motorcycles)
+
+l1 = [9, 3, 45, 2, 1, 6, 9, 9]
+l1.sort()
+print(l1)
+l1.reverse()
+print(l1)
+print(l1.count(9))
+
+# motorcycles.sort()
+print(motorcycles.index('apple'))
