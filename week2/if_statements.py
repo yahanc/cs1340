d48e669451ac22a180c3674f21cabe35c2e3da6d
Xinyi Ding <dxywill@gmail.com>
2019-09-08 13:49:35 -0500

    week2 lec2

---
+++ week2/if_statements.py
@@ -0,0 +1,31 @@
+# age = 17
+# if age >= 18:
+#     print("You are old enough to vote!")
+#     print("Have you registered yet?")
+# else:
+#     print("Sorry, you are too young to vote")
+#     print("Please register to vote")
+
+
+# age = 100
+#
+# if age < 4:
+#     price = 0
+# elif age < 18:
+#     price = 5
+# elif age < 65:
+#     price = 8
+# else:
+#     price = 90
+#
+# print("Your admission cost is $" + str(price))
+
+requested_topping = {}
+
+
+this_is_number = 8
+
+if this_is_number:
+    print("This is True")
+else:
+    print("This is False")
