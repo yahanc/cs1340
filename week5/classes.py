5885e8ae44ccff46e440b5220726d7f79bbf4ce5
Xinyi Ding <dxywill@gmail.com>
2019-09-27 10:31:37 -0500

    week5 lec3

--- week5/classes.py
+++ week5/classes.py
@@ -1,23 +1,25 @@
 class Dog():
     """A simple attempt to model a dog"""
 
-    def __init__(self, name, age, weight):
+    def __init__(self, name, age, weight, height):
 
         """Initialize name and age attributes """
         self.name = name
         self.age = age
         self.weight = weight
+        self.height = height
 
     def sit(self):
         """Simulating a dog sitting"""
         # print(self.weight)
+        print(self.height)
         print(self.name + " is sitting now")
 
     def roll_over(self):
         print(self.name + " is rolled over")
 
 
-# my_dog = Dog("shadow", 6, 100)
+# my_dog = Dog("shadow", 6, 100, 9)
 # my_dog.sit()
 # my_dog.roll_over()
 #
@@ -53,10 +55,43 @@ class Car():
             self.odometer_reading = mileage
 
 my_car = Car("bmw", "m3", 2016)
-my_car.get_descriptive_name()
 
-print(my_car.get_odometer_reading())
-my_car.update_odometer_reading(1000)
-print(my_car.get_odometer_reading())
 
-my_car.update_odometer_reading(-500)
+
+# my_car.get_descriptive_name()
+#
+# print(my_car.get_odometer_reading())
+# my_car.update_odometer_reading(1000)
+# print(my_car.get_odometer_reading())
+#
+# my_car.update_odometer_reading(-500)
+
+
+class Battery():
+
+
+
+
+class ElectricCar(Car):
+    """This is an electirc car"""
+
+    def __init__(self, make, model, year, battery):
+        super().__init__(make, model, year)
+        # self.make = make
+        # self.model = model
+        # self.year = year
+        self.battery = Battery()
+
+    def get_electric_descriptive(self):
+        print(self.year)
+        print(self.make)
+        print(self.model)
+        print(self.battery)
+
+    def get_odometer_reading(self):
+        print("this is a new odometer reading method")
+
+
+
+my_tesla = ElectricCar("tesla", "model s", 2019, "this is a battery")
+my_tesla.get_odometer_reading()
